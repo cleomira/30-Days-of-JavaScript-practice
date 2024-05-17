0244764ed3484724df42f8c6708deb1309a28752
Ngozi Cleopatra Ugwu <ugwungozicleopatra@gmail.com>
2024-05-17 13:09:13 +0100

    New Update

--- Day 2/Test.js
+++ Day 2/Test.js
@@ -11,8 +11,20 @@ console.log(challenge.split(' '))// split to an array at space
 let socialMediaPlatforms = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
 console.log(socialMediaPlatforms.split(','))  // split to any array at comma 
 console.log(socialMediaPlatforms.split(', '))// changes to an array
-console.log(challenge.replace('JavaScript', 'Python'))
-console.log(challenge.charAt(15)) 
-console.log(challenge.charCodeAt(11))  
-console.log(challenge.indexOf('a')) 
-console.log(challenge.lastIndexOf('a'))
+console.log(challenge.replace('JavaScript', 'Python'))// replacing JavaScript with python
+console.log(challenge.charAt(15))// finding the character at index 15
+console.log(challenge.charCodeAt(11))// finding the character code J 
+console.log(challenge.indexOf('a'))// finding the position of first occurence of "a"
+console.log(challenge.lastIndexOf('a'))// finding the position of first occurence of "a"
+let sentence = "You cannot end a sentence with because because because is a conjunction"
+console.log(sentence.indexOf('because'))// finding the position of first occurence of "because"
+console.log(sentence.lastIndexOf('because'))// finding the position of first occurence of "because"
+console.log(sentence.search('because'))// finding the position of first occurence of "because"
+let selfPraise = ' I am an awesome being '
+console.log(selfPraise.trim(' '))
+console.log(challenge.startsWith('30'))
+console.log(challenge.endsWith('JavaScript'))
+let patternOne = /a/gi
+console.log(challenge.match(patternOne))
+let string = '30 Days Of'
+console.log(string.concat("JavaScript"))
